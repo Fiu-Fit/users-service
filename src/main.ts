c81c5d2b3276e72443bcdcd214008a9f3c07f656
mmedela <mmedela@fi.uba.ar>
2023-04-03 18:37:52 -0300

    test github actions try 5

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//add comment to test actions
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
