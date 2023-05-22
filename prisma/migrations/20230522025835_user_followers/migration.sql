f7cc33fe6895319a8ef693510a6a5fd588097b29
Rocío <rplatini@fi.uba.ar>
2023-05-22 10:09:09 -0300

    Create relations between User and Follower

--- prisma/migrations/20230522025835_user_followers/migration.sql
+++ prisma/migrations/20230522025835_user_followers/migration.sql
@@ -11,11 +11,14 @@ ADD COLUMN     "favorite_workouts" TEXT[] DEFAULT ARRAY[]::TEXT[];
 -- CreateTable
 CREATE TABLE "Follower" (
     "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "follower_id" INTEGER NOT NULL,
-    "following_id" INTEGER NOT NULL,
 
     CONSTRAINT "Follower_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Follower_follower_id_following_id_key" ON "Follower"("follower_id", "following_id");
+CREATE UNIQUE INDEX "Follower_user_id_follower_id_key" ON "Follower"("user_id", "follower_id");
+
+-- AddForeignKey
+ALTER TABLE "Follower" ADD CONSTRAINT "Follower_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
