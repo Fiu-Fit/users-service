cd1a32040c886aee9549258478151bc9d00bc277
Rocío <rplatini@fi.uba.ar>
2023-05-23 14:50:11 -0300

    User followers (#38)
    * Create Follower schema
    
    * Register Follower module
    
    * Create Follower service
    
    * Create new endpoints
    
    * Create relations between User and Follower
    
    * Apply changes in service
    
    * Fix controller
    
    * Create Follower controller
    
    * Fix issue
    
    * Delete followedAt field
    
    * Rename query from `id` to `userId`
    
    ---------
    
    Co-authored-by: Ian Shih <ianshih2003@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { FollowerModule } from './modules/followers/follower.module';
 import { MetricsModule } from './modules/metrics/metrics.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports:     [ConfigModule.forRoot(), UserModule, AuthModule, MetricsModule],
+  imports:     [ConfigModule.forRoot(), UserModule, AuthModule, FollowerModule, MetricsModule],
   controllers: [AppController],
   providers:   [AppService],
 })
